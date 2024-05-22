c35d6115786aa917d282fafb94f1f4dd8ea0ff24
tan <83101100+minhtan11@users.noreply.github.com>
2024-05-23 06:48:41 +0700

    .

--- src/app/main/setting-page/report-page/report-page.component.ts
+++ src/app/main/setting-page/report-page/report-page.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Keyboard } from '@capacitor/keyboard';
 import { NavController } from '@ionic/angular';
+import { Subject, takeUntil } from 'rxjs';
 import { ApiserviceComponent } from 'src/app/apiservice/apiservice.component';
 import { NotificationServiceComponent } from 'src/app/notification-service/notification-service.component';
 import { StorageService } from 'src/app/storage-service/storage.service';
@@ -14,7 +15,12 @@ import { StorageService } from 'src/app/storage-service/storage.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReportPageComponent  implements OnInit {
+  @ViewChild('eleType') eleType: any;
+  @ViewChild('eleTransId') eleTransId: any;
+  @ViewChild('eleContext') eleContext: any;
   isHideFooter:any=false;
+  formGroup!: FormGroup;
+  private destroy$ = new Subject<void>();
   constructor(
     private dt: ChangeDetectorRef,
     private api: ApiserviceComponent,
@@ -26,7 +32,18 @@ export class ReportPageComponent  implements OnInit {
   ) { 
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      type: ['',Validators.required],
+      transId: ['', Validators.required],
+      context: ['', Validators.required],
+      status: [1],
+      createdBy:[''],
+      createdDate:[Date.now()]
+    });
+    let username = await this.storage.get('username');
+    this.formGroup.patchValue({createdBy:username});
+  }
 
   async ngAfterViewInit() {
     Keyboard.addListener('keyboardWillShow', info => {
@@ -45,7 +62,39 @@ export class ReportPageComponent  implements OnInit {
   }
 
   onReport(){
-
+    if (this.formGroup.controls['type'].invalid) {
+      this.notification.showNotiError('', 'Vui lòng chọn hình thức khiếu nại!');
+      this.eleType.nativeElement.focus();
+      return;
+    }
+    if (this.formGroup.controls['tranId'].invalid) {
+      this.notification.showNotiError('', 'Mã đơn hàng không được bỏ trống!');
+      this.eleTransId.nativeElement.focus();
+      return;
+    }
+    if (this.formGroup.controls['context'].invalid) {
+      this.notification.showNotiError('', 'Nội dung không được bỏ trống!');
+      this.eleContext.nativeElement.focus();
+      return;
+    }
+    this.api.isLoad(true);
+    setTimeout(async () => {
+      this.api.execByBody('Authencation','createcomplain',this.formGroup.value).pipe(takeUntil(this.destroy$)).subscribe({
+        next:(res:any)=>{
+          if (res && !res?.isError) {
+            this.notification.showNotiSuccess('', res.message);
+            this.navCtrl.navigateForward('main',{queryParams:{selected:3}});
+            this.dt.detectChanges();
+          }else{
+            this.notification.showNotiError('',res?.message);
+            this.dt.detectChanges();
+          }
+        },
+        complete:()=>{
+          this.api.isLoad(false);
+        }
+      })
+    }, 1000);
   }
 
 }
