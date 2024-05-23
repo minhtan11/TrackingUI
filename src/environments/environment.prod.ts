4458c82acb6f3e42756d2bfe4a516d959876e072
tmtan <tmtan@lacviet.com.vn>
2024-05-23 18:22:11 +0700

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl:'http://localhost:7043/api/',
+  //apiUrl:'http://localhost:7043/api/',
   //apiUrl:'https://172.16.9.122:45455/api/',
-  //apiUrl:'https://tracking.nhanshiphang.vn/api/',
+  apiUrl:'https://tracking.nhanshiphang.vn/api/',
   //apiUrl:'https://192.168.2.8:45455/api/',
 };
