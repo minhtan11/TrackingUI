ad01aefc73296097004785270e1296af767271a2
tmtan <minhtan.tn11@gmail.com>
2024-07-03 06:40:07 +0700

    update

--- ios/App/NotificationServiceExtension/NotificationService.swift
+++ ios/App/NotificationServiceExtension/NotificationService.swift
@@ -1,35 +1,22 @@
-//
-//  NotificationService.swift
-//  NotificationServiceExtension
-//
-//  Created by MINHTAN on 3/7/24.
-//
-
 import UserNotifications
+import FirebaseMessaging
 
 class NotificationService: UNNotificationServiceExtension {
-
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        guard let content = request.content.mutableCopy() as? UNMutableNotificationContent else { return }
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        self.bestAttemptContent = content
         
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
+        FIRMessagingExtensionHelper().populateNotificationContent(content, withContentHandler: contentHandler)
     }
     
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
+        guard let contentHandler = contentHandler,
+              let bestAttemptContent =  bestAttemptContent else { return }
+        
+        contentHandler(bestAttemptContent)
     }
-
 }
