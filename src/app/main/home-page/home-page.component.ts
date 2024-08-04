4a87829417911f292f3f7c93052c37bb4f3aa10c
tmtan <minhtan.tn11@gmail.com>
2024-08-04 09:31:12 +0700

    .

--- src/app/main/home-page/home-page.component.ts
+++ src/app/main/home-page/home-page.component.ts
@@ -41,7 +41,7 @@ export class HomePageComponent{
   isPopup:any=false;
   isPopupVersion:any=false;
   imgPopup:any;
-  versionNo:any='1.9';
+  versionNo:any='2.0';
   textCopy:any = '';
   isOpenCopy:any=false;
   private destroy$ = new Subject<void>();
