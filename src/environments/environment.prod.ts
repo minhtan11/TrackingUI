10c02e2351c167f0bb6173b805fcf40ea2764b72
tmtan <minhtan.tn11@gmail.com>
2024-08-11 13:46:54 +0700

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl:'https://localhost:7043/api/',
+  //apiUrl:'https://localhost:7043/api/',
   //apiUrl:'https://192.168.2.13:45455/api/',
-  //apiUrl:'https://tracking.nhanshiphang.vn/api/',
+  apiUrl:'https://tracking.nhanshiphang.vn/api/',
   //apiUrl:'https://192.168.2.8:45455/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl:'https://localhost:7043/api/',
+  //apiUrl:'https://localhost:7043/api/',
   //apiUrl:'https://192.168.2.13:45455/api/',
   //apiUrl:'https://192.168.2.8:45455/api/',
-  //apiUrl:'https://tracking.nhanshiphang.vn/api/',
+  apiUrl:'https://tracking.nhanshiphang.vn/api/',
 };
 
 /*
