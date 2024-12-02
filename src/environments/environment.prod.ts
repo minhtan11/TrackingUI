288ce46163936f75660cc57813fb627a5cd74069
tmtan <minhtan.tn11@gmail.com>
2024-12-03 00:42:00 +0700

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl:'https://localhost:7043/api/',
+  //apiUrl:'https://localhost:7043/api/',
   //apiUrl:'https://192.168.2.13:45455/api/',
-  //apiUrl:'https://tracking.nhanshiphang.vn/api/',
+  apiUrl:'https://tracking.nhanshiphang.vn/api/',
   //apiUrl:'https://192.168.2.8:45455/api/',
   version:'3.1'
 };
