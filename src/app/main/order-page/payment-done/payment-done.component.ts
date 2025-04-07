678a88945f7fec8dd56ab47a9110187670dc242c
tmtan <minhtan.tn11@gmail.com>
2025-04-08 06:17:49 +0700

    .

--- src/app/main/order-page/payment-done/payment-done.component.ts
+++ src/app/main/order-page/payment-done/payment-done.component.ts
@@ -37,111 +37,211 @@ export class PaymentDoneComponent  implements OnInit {
       case 'ios':
         const scheme = 'grab://';
         const fallback = 'https://apps.apple.com/vn/app/grab-%C4%91%E1%BA%B7t-xe-giao-%C4%91%E1%BB%93-%C4%83n/id647268330?l=vi';
-
         const now = Date.now();
         window.location.href = scheme;
-
         setTimeout(() => {
           const elapsed = Date.now() - now;
           if (elapsed < 1500) {
             window.location.href = fallback;
           }
         }, 1000);
-        // await Browser.open({
-        //   url: 'https://grab.onelink.me/2695613898?af_banner=true&pid=organic_web&c=organic_web&af_adset=grab_website&af_ad=vn&af_channel=smart_banner_pax&af_sub1=open_app&is_retargeting=true&af_dp=grab%3A%2F%2Fopen%3FscreenType%3DMAIN&af_force_deeplink=true&af_sub5=organic',
-        // });
         break;
     }
   }
 
   async openAhamove(){
-    let url = this.platform === 'android' ? 'com.ahamove.user' : 'ahamove://';
-    const { value } = await AppLauncher.canOpenUrl({ url: url });
-    if(value){
-      await AppLauncher.openUrl({ url: url });
-    }else{
-      if(this.platform === 'android')
-        await Browser.open({ url: 'https://play.google.com/store/apps/details?id=com.ahamove.user&hl=vi' });
-      else
-        await Browser.open({ url: 'https://apps.apple.com/vn/app/ahamove-chuy%C3%AAn-gia-giao-h%C3%A0ng/id987325355?l=vi' });
+    switch(this.platform){
+      case 'android':
+        let url = 'com.ahamove.user'
+        const { value } = await AppLauncher.canOpenUrl({ url: url });
+        if (value) {
+          await AppLauncher.openUrl({ url: url });
+        } else {
+          await Browser.open({
+            url: 'https://play.google.com/store/apps/details?id=com.ahamove.user&hl=vi',
+          });
+        }
+        break;
+      case 'ios':
+        const scheme = 'ahamove://';
+        const fallback = 'https://apps.apple.com/vn/app/ahamove-chuy%C3%AAn-gia-giao-h%C3%A0ng/id987325355?l=vi';
+        const now = Date.now();
+        window.location.href = scheme;
+        setTimeout(() => {
+          const elapsed = Date.now() - now;
+          if (elapsed < 1500) {
+            window.location.href = fallback;
+          }
+        }, 1000);
+        break;
     }
   }
 
   async openXanhSM(){
-    let url = this.platform === 'android' ? 'com.gsm.customer' : 'xanhsm.com://';
-    const { value } = await AppLauncher.canOpenUrl({ url: url });
-    if(value){
-      await AppLauncher.openUrl({ url: url });
-    }else{
-      if(this.platform === 'android')
-        await Browser.open({ url: 'https://play.google.com/store/apps/details?id=com.gsm.customer&hl=vi' });
-      else
-        await Browser.open({ url: 'https://apps.apple.com/vn/app/xanh-sm-%C4%91%E1%BA%B7t-xe-%C4%91i%E1%BB%87n/id6446425595?l=vi' });
+    switch(this.platform){
+      case 'android':
+        let url = 'com.gsm.customer'
+        const { value } = await AppLauncher.canOpenUrl({ url: url });
+        if (value) {
+          await AppLauncher.openUrl({ url: url });
+        } else {
+          await Browser.open({
+            url: 'https://play.google.com/store/apps/details?id=com.gsm.customer&hl=vi',
+          });
+        }
+        break;
+      case 'ios':
+        const scheme = 'xanhsm.com://';
+        const fallback = 'https://apps.apple.com/vn/app/xanh-sm-%C4%91%E1%BA%B7t-xe-%C4%91i%E1%BB%87n/id6446425595?l=vi';
+        const now = Date.now();
+        window.location.href = scheme;
+        setTimeout(() => {
+          const elapsed = Date.now() - now;
+          if (elapsed < 1500) {
+            window.location.href = fallback;
+          }
+        }, 1000);
+        break;
     }
   }
 
   async openBE(){
-    let url = this.platform === 'android' ? 'xyz.be.customer' : 'be://';
-    const { value } = await AppLauncher.canOpenUrl({ url: url });
-    if(value){
-      await AppLauncher.openUrl({ url: url });
-    }else{
-      if(this.platform === 'android')
-        await Browser.open({ url: 'https://play.google.com/store/apps/details?id=xyz.be.customer&hl=vi' });
-      else
-        await Browser.open({ url: 'https://apps.apple.com/vn/app/be-n%E1%BB%81n-t%E1%BA%A3ng-%C4%91a-d%E1%BB%8Bch-v%E1%BB%A5/id1440565902?l=vi' });
+    switch(this.platform){
+      case 'android':
+        let url = 'xyz.be.customer'
+        const { value } = await AppLauncher.canOpenUrl({ url: url });
+        if (value) {
+          await AppLauncher.openUrl({ url: url });
+        } else {
+          await Browser.open({
+            url: 'https://play.google.com/store/apps/details?id=xyz.be.customer&hl=vi',
+          });
+        }
+        break;
+      case 'ios':
+        const scheme = 'be://';
+        const fallback = 'https://apps.apple.com/vn/app/be-n%E1%BB%81n-t%E1%BA%A3ng-%C4%91a-d%E1%BB%8Bch-v%E1%BB%A5/id1440565902?l=vi';
+        const now = Date.now();
+        window.location.href = scheme;
+        setTimeout(() => {
+          const elapsed = Date.now() - now;
+          if (elapsed < 1500) {
+            window.location.href = fallback;
+          }
+        }, 1000);
+        break;
     }
   }
 
   async openLalamove(){
-    let url = this.platform === 'android' ? 'hk.easyvan.app.client' : 'be://';
-    const { value } = await AppLauncher.canOpenUrl({ url: url });
-    if(value){
-      await AppLauncher.openUrl({ url: url });
-    }else{
-      if(this.platform === 'android')
-        await Browser.open({ url: 'https://play.google.com/store/apps/details?id=hk.easyvan.app.client&hl=vi' });
-      else
-        await Browser.open({ url: 'https://apps.apple.com/vn/app/lalamove-30-gi%C3%A2y-l%C3%A0-c%C3%B3-xe/id735701965?l=vi' });
+    switch(this.platform){
+      case 'android':
+        let url = 'hk.easyvan.app.client'
+        const { value } = await AppLauncher.canOpenUrl({ url: url });
+        if (value) {
+          await AppLauncher.openUrl({ url: url });
+        } else {
+          await Browser.open({
+            url: 'https://play.google.com/store/apps/details?id=hk.easyvan.app.client&hl=vi',
+          });
+        }
+        break;
+      case 'ios':
+        const scheme = 'lalamove://';
+        const fallback = 'https://apps.apple.com/vn/app/lalamove-30-gi%C3%A2y-l%C3%A0-c%C3%B3-xe/id735701965?l=vi';
+        const now = Date.now();
+        window.location.href = scheme;
+        setTimeout(() => {
+          const elapsed = Date.now() - now;
+          if (elapsed < 1500) {
+            window.location.href = fallback;
+          }
+        }, 1000);
+        break;
     }
   }
 
   async openGHN(){
-    let url = this.platform === 'android' ? 'vn.ghn.app.giaohangnhanh' : 'be://';
-    const { value } = await AppLauncher.canOpenUrl({ url: url });
-    if(value){
-      await AppLauncher.openUrl({ url: url });
-    }else{
-      if(this.platform === 'android')
-        await Browser.open({ url: 'https://play.google.com/store/apps/details?id=vn.ghn.app.giaohangnhanh&hl=vi' });
-      else
-        await Browser.open({ url: 'https://apps.apple.com/vn/app/ghn-giao-h%C3%A0ng-nhanh/id1203171490?l=vi' });
+    switch(this.platform){
+      case 'android':
+        let url = 'vn.ghn.app.giaohangnhanh'
+        const { value } = await AppLauncher.canOpenUrl({ url: url });
+        if (value) {
+          await AppLauncher.openUrl({ url: url });
+        } else {
+          await Browser.open({
+            url: 'https://play.google.com/store/apps/details?id=vn.ghn.app.giaohangnhanh&hl=vi',
+          });
+        }
+        break;
+      case 'ios':
+        const scheme = 'ghn://';
+        const fallback = 'https://apps.apple.com/vn/app/ghn-giao-h%C3%A0ng-nhanh/id1203171490?l=vi';
+        const now = Date.now();
+        window.location.href = scheme;
+        setTimeout(() => {
+          const elapsed = Date.now() - now;
+          if (elapsed < 1500) {
+            window.location.href = fallback;
+          }
+        }, 1000);
+        break;
     }
   }
 
   async openGHTK(){
-    let url = this.platform === 'android' ? 'vn.giaohangtietkiem.ghtk_pro' : 'be://';
-    const { value } = await AppLauncher.canOpenUrl({ url: url });
-    if(value){
-      await AppLauncher.openUrl({ url: url });
-    }else{
-      if(this.platform === 'android')
-        await Browser.open({ url: 'https://play.google.com/store/apps/details?id=vn.giaohangtietkiem.ghtk_pro&hl=vi' });
-      else
-        await Browser.open({ url: 'https://apps.apple.com/vn/app/ghtk/id1131996979?l=vi' });
+    switch(this.platform){
+      case 'android':
+        let url = 'vn.giaohangtietkiem.ghtk_pro'
+        const { value } = await AppLauncher.canOpenUrl({ url: url });
+        if (value) {
+          await AppLauncher.openUrl({ url: url });
+        } else {
+          await Browser.open({
+            url: 'https://play.google.com/store/apps/details?id=vn.giaohangtietkiem.ghtk_pro&hl=vi',
+          });
+        }
+        break;
+      case 'ios':
+        const scheme = 'ghtk://';
+        const fallback = 'https://apps.apple.com/vn/app/ghtk/id1131996979?l=vi';
+        const now = Date.now();
+        window.location.href = scheme;
+        setTimeout(() => {
+          const elapsed = Date.now() - now;
+          if (elapsed < 1500) {
+            window.location.href = fallback;
+          }
+        }, 1000);
+        break;
     }
   }
 
   async openJT(){
-    let url = this.platform === 'android' ? 'com.jt.customer.vietnam' : 'be://';
-    const { value } = await AppLauncher.canOpenUrl({ url: url });
-    if(value){
-      await AppLauncher.openUrl({ url: url });
-    }else{
-      if(this.platform === 'android')
-        await Browser.open({ url: 'https://play.google.com/store/apps/details?id=com.jt.customer.vietnam&hl=vi' });
-      else
-        await Browser.open({ url: 'https://apps.apple.com/vn/app/j-t-express-vn/id6449207307?l=vi' });
+    switch(this.platform){
+      case 'android':
+        let url = 'com.jt.customer.vietnam'
+        const { value } = await AppLauncher.canOpenUrl({ url: url });
+        if (value) {
+          await AppLauncher.openUrl({ url: url });
+        } else {
+          await Browser.open({
+            url: 'https://play.google.com/store/apps/details?id=com.jt.customer.vietnam&hl=vi',
+          });
+        }
+        break;
+      case 'ios':
+        const scheme = 'jtexpress://';
+        const fallback = 'https://apps.apple.com/vn/app/j-t-express-vn/id6449207307?l=vi';
+        const now = Date.now();
+        window.location.href = scheme;
+        setTimeout(() => {
+          const elapsed = Date.now() - now;
+          if (elapsed < 1500) {
+            window.location.href = fallback;
+          }
+        }, 1000);
+        break;
     }
   }
 }
